0e605eb74c1e7e5e3eec15832f7b9956b13f9050
apoidomani <81364419+apoidomani@users.noreply.github.com>
2021-03-25 10:06:08 -0300

    Add files via upload

---
+++ ejercicio2Poidomani.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc06
+ */
+public class ejercicio2Poidomani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=5;
+        int suma =a+b;
+        int resta=a-b;
+        int multiplicacion =a*b;
+        int division= a/b;
+        System.out.println("la suma de" + a + "mas" +b+ " es igual a " + suma );
+        System.out.println("la resta de" + a + "mas" +b+ "es igual a " + resta);
+        System.out.println("la multiplicacion de " + a + "por" +b+ "es igual a " + multiplicacion);
+        System.out.println("la division de" +a+ "dividido a" +b+ "es igual a " +division);
+        
+    }
+    
+}
