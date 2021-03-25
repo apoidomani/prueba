8932be65339a1213f14bd47794656f23d272e777
apoidomani <81364419+apoidomani@users.noreply.github.com>
2021-03-25 09:32:20 -0300

    Add files via upload

---
+++ Ejercicio1Poidomani.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg1.poidomani;
+
+/**
+ *
+ * @author pc06
+ */
+public class Ejercicio1Poidomani {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=4;
+        int b=7;
+        int res;
+        res=a+b;
+        System.out.println("el resultado es " +res);
+                
+    }
+    
+}
