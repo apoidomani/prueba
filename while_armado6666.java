1801e4877f18efd635ea0002baa4a899ba6660d2
apoidomani <81364419+apoidomani@users.noreply.github.com>
2021-04-22 10:31:45 -0300

    Add files via upload

---
+++ while_armado6666.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionalespoidomani;
+import java.util.Scanner;
+/**
+ *
+ * @author pc06
+ */
+public class while_armado6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=newScanner (System.in);
+        int num=0;
+        System.out.println("ingrese numero maximo");
+        num=entrada.nextInt();
+        do {System.out.println(num);
+        num++;
+        while (num<=num;{
+            if (num%6==0) {
+                System.out.println(num);
+            }
+        }
+        }
+        
+    }
+    
+}
