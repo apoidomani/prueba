9b5f06b4cb43bf6c535e571305425669bb1fd2c0
apoidomani <81364419+apoidomani@users.noreply.github.com>
2021-04-22 09:56:08 -0300

    Add files via upload

---
+++ while_armado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionalespoidomani;
+
+/**
+ *
+ * @author pc06
+ */
+public class while_armado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=1;
+    while (num<2000) {
+        if (num%10==0) {
+            System.out.println(num);
+        }
+    }
+    num++;
+    }
+    }
+    
+    
+    
